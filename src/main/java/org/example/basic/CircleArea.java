8474ba892160938ac3f7c777a0fe196e17d1eda6
AIWS-203-112 <mabest123@naver.com>
2025-05-13 09:24:32 +0900

    산술 연산 연습

--- src/main/java/org/example/basic/CircleArea.java
+++ src/main/java/org/example/basic/CircleArea.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.basic;
 
 public class CircleArea {
     public static void main(String[] args) {
--- src/main/java/org/example/basic/TypeConversation.java
+++ src/main/java/org/example/basic/TypeConversation.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.basic;
 
 public class TypeConversation {
     public static void main(String[] args) {
