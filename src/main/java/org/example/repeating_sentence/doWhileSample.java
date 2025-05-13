89c6bb1e0eb409d5727bd956179d4f6ceb823284
AIWS-203-112 <mabest123@naver.com>
2025-05-13 15:11:56 +0900

    자바 DoWhile문 연습

---
+++ src/main/java/org/example/repeating_sentence/doWhileSample.java
@@ -0,0 +1,12 @@
+package org.example.repeating_sentence;
+
+public class doWhileSample {
+    public static void main(String[] args) {
+        char c = 'a';
+
+        do {
+            System.out.print(c);
+            c = (char) (c + 1);
+        } while (c <= 'z');
+    }
+}
