5a5f31b13be22807a0eebd17a8514286c2cf00d9
AIWS-203-112 <mabest123@naver.com>
2025-05-20 15:58:06 +0900

    오브젝트 실습

---
+++ src/main/java/org/example/Object/Point.java
@@ -0,0 +1,19 @@
+package org.example.Object;
+
+public class Point {
+    private int x, y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "Point(" + x + "," + y + ")";
+    }
+
+    public boolean equals(Object obj) {
+        Point p = (Point)obj;
+        if(x == p.x && y == p.y) return true;
+        else return false;
+    }
+}
